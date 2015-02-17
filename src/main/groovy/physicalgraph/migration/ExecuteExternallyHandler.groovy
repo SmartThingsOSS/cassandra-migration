c6f8b200cfacf1a65545bbf9fad553b37a6fb4fc
Shaun Jurgemeyer <sjurgemeyer@gmail.com>
2015-02-17 12:38:25 -0600

    Using host parameter

--- src/main/groovy/physicalgraph/migration/ExecuteExternallyHandler.groovy
+++ src/main/groovy/physicalgraph/migration/ExecuteExternallyHandler.groovy
@@ -11,7 +11,7 @@ class ExecuteExternallyHandler {
 		String md5 = Util.calculateMd5(file.text)
 		String existingMd5 = connection.getMigrationMd5(file.name)
 		if (existingMd5 && md5 == existingMd5) {
-				println "${file.name} was already run"
+			println "${file.name} was already run"
 		} else if (existingMd5 && !parameters.override){
 			throw new Exception("ERROR! md5 of ${file.name} is different from the last time it was run!")
 		} else {
@@ -19,7 +19,7 @@ class ExecuteExternallyHandler {
 
 			connection.markMigration(file, md5)
 
-			String command = "${parameters.location} -f ${file.absolutePath} -k ${parameters.keyspace}"
+			String command = "${parameters.location} -f ${file.absolutePath} -k ${parameters.keyspace} -h ${parameters.host}"
 			println command
 			command.execute()
 		}
