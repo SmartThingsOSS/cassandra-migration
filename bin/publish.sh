c564d6a2b746465d157780c6e2f1dec93e03b93e
Jeff Beck <beckje01@gmail.com>
2015-12-03 11:40:35 -0600

    Publish to bintray only on release branch

--- bin/publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eu
-
-if grep -q SNAPSHOT "version.txt"; then
-   echo "Publishing snapshot to internal artifactory"
-   ./gradlew publish -PsmartThingsArtifactoryUserName=$ARTIFACTORY_USERNAME -PsmartThingsArtifactoryPassword=$ARTIFACTORY_PASSWORD
-else
-   echo "Publishing release version to bintray"
-   ./gradlew bintrayUpload
-fi
