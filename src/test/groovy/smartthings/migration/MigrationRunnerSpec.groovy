f1054da37e9e677f088ab1e9f0ebc6764481f4c8
Jeff Beck <beckje01@gmail.com>
2016-08-05 11:21:50 -0500

    Allow Giving a Session in Settings
    It will not try and shutdown a provided session.
    
    This fixes #4

--- src/test/groovy/smartthings/migration/MigrationRunnerSpec.groovy
+++ src/test/groovy/smartthings/migration/MigrationRunnerSpec.groovy
@@ -1,5 +1,6 @@
 package smartthings.migration
 
+import com.datastax.driver.core.Cluster
 import com.datastax.driver.core.ResultSet
 import org.cassandraunit.CassandraCQLUnit
 import org.cassandraunit.dataset.CQLDataSet
@@ -17,7 +18,7 @@ class MigrationRunnerSpec extends Specification {
 
 	def setup() {
 		CQLDataSet dataSet = new ClassPathCQLDataSet('test-baseline.cql', keyspace)
-		cassandraCQLUnit = new CassandraCQLUnit(dataSet, 'test-cassandra.yaml')
+		cassandraCQLUnit = new CassandraCQLUnit(dataSet, 'test-cassandra.yaml', 30000)
 		cassandraCQLUnit.before()
 	}
 
@@ -29,11 +30,11 @@ class MigrationRunnerSpec extends Specification {
 		given:
 
 		def params = new MigrationParameters.Builder()
-				.setHost('localhost')
-				.setPort(9142)
-				.setKeyspace(keyspace)
-				.setMigrationsLogFile('/cassandra/success.changelog')
-				.build()
+			.setHost('localhost')
+			.setPort(9142)
+			.setKeyspace(keyspace)
+			.setMigrationsLogFile('/cassandra/success.changelog')
+			.build()
 
 		CassandraConnection connection = new CassandraConnection(params)
 		connection.connect()
@@ -60,22 +61,67 @@ class MigrationRunnerSpec extends Specification {
 		def migrations2 = processRows(connection.execute('SELECT * from migrations'))
 
 		migrations2 == [
-				[name: 'change-1.cql', sha: '834bd37fb41f231e3df36dcd2c51afda'],
-				[name: 'change-2.cql', sha: 'd00f97fc30b437bf887a2d7eb557f2c5'],
+			[name: 'change-1.cql', sha: '834bd37fb41f231e3df36dcd2c51afda'],
+			[name: 'change-2.cql', sha: 'd00f97fc30b437bf887a2d7eb557f2c5'],
 		]
 
 		def data2 = processRows(connection.execute('SELECT * FROM a')).sort()
-		data2 == [[id: '1', value: 'success'],[id: '2', value: 'success-2']]
+		data2 == [[id: '1', value: 'success'], [id: '2', value: 'success-2']]
+	}
+
+	def 'run migrations via session'() {
+		given:
+
+		Cluster.Builder builder = Cluster.builder().addContactPoint('localhost').withPort(9142);
+
+
+		def params = new MigrationParameters.Builder()
+			.setSession(builder.build().connect())
+			.setKeyspace(keyspace)
+			.setMigrationsLogFile('/cassandra/success.changelog')
+			.build()
+
+		CassandraConnection connection = new CassandraConnection(params)
+		connection.connect()
+
+		when:
+		runner.run(params)
+
+		then: 'the migrations table exists and has an entry'
+
+		def migrations = processRows(connection.execute('SELECT * from migrations'))
+
+		migrations == [[name: 'change-1.cql', sha: '834bd37fb41f231e3df36dcd2c51afda']]
+
+		and: 'the migration data is present'
+
+		def data = processRows(connection.execute('SELECT * FROM a'))
+		data == [[id: '1', value: 'success']]
+
+		when: 'run a second migration'
+		params.migrationsLogFile = '/cassandra/success2.changelog'
+		runner.run(params)
+
+		then: 'new migration is there too and data is added'
+		def migrations2 = processRows(connection.execute('SELECT * from migrations'))
+
+		migrations2 == [
+			[name: 'change-1.cql', sha: '834bd37fb41f231e3df36dcd2c51afda'],
+			[name: 'change-2.cql', sha: 'd00f97fc30b437bf887a2d7eb557f2c5'],
+		]
+
+		def data2 = processRows(connection.execute('SELECT * FROM a')).sort()
+		data2 == [[id: '1', value: 'success'], [id: '2', value: 'success-2']]
 	}
 
 	def 'run failing migration'() {
 		given:
 		def params = new MigrationParameters.Builder()
-				.setHost('localhost')
-				.setPort(9142)
-				.setKeyspace(keyspace)
-				.setMigrationsLogFile('/cassandra/failure.changelog')
-				.build()
+			.setHost('localhost')
+			.setPort(9142)
+			.setKeyspace(keyspace)
+			.setMigrationsLogFile('/cassandra/failure.changelog')
+			.build()
 
 		CassandraConnection connection = new CassandraConnection(params)
 		connection.connect()
@@ -94,7 +140,7 @@ class MigrationRunnerSpec extends Specification {
 		and: 'the migration data is present from the successful migration and the failed!'
 		//TODO: batch would allow rollback
 		def data = processRows(connection.execute('SELECT * FROM a')).sort()
-		data == [[id: '1', value: 'success'],[id: '3', value: 'fail-added']]
+		data == [[id: '1', value: 'success'], [id: '3', value: 'fail-added']]
 	}
 
 	List<Map> processRows(ResultSet results) {
